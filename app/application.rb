34998c9a250ca7e7c9c1c73b443e73ee845e8a47
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-05 16:49:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,10 @@ class Application
     item = req.path.split("/items/").last
     item_from_cart = @@items.find{|i|i.name == item}
     resp.write item_from_cart.price
-  elsif   @@items.none{|i|i.name == item} 
+  elsif   @@items.none{|i|i.name == item}
     resp.write "Item not there"
     resp.status = 400
-       
+
   else
     resp.write "Route not found"
     resp.status = 404
