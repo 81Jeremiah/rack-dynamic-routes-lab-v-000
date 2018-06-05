7b767f962f8637a1a69e7f26f1efebc6b4d5ddb6
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-05 16:22:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+  if req.path.match(/items/)
     item = req.path.split("/items/").last
 
+  else
+    resp.write "Route not found"
+    resp.status = 404
+  end
   # if req.path==""
     resp.finish
   end
