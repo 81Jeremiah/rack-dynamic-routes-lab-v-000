ce9b956c01984419df0981346b04f5de710962b2
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-05 16:54:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,14 +5,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
   if req.path.match(/items/)
     item = req.path.split("/items/").last
-    item_from_cart = @@items.find{|i|i.name == item}
-    resp.write item_from_cart.price
-  elsif   @@items.none?{|i|i.name == item}
+    if  
+      item_from_cart = @@items.find{|i|i.name == item}
+      resp.write item_from_cart.price
+    else   
     resp.write "Item not there"
     resp.status = 400
-
+    end
   else
     resp.write "Route not found"
     resp.status = 404
